cdee650fd0a125e31ba64079b08203aa079d34a0
Vincent Rémond <vincent.remond@gmail.com>
2024-08-01 14:57:09 +0200

    fix: on garde la dernière version des nuget en cas de doublon

--- CentralPackageManagement/Program.fs
+++ CentralPackageManagement/Program.fs
@@ -3,6 +3,7 @@ open System.IO
 open System.Text
 open System.Xml
 open System.Xml.Linq
+open FsToolkit.ErrorHandling
 
 type PackageId =
     | PackageId of string
@@ -11,12 +12,64 @@ type PackageId =
         match this with
         | PackageId value -> value
 
-type Version =
-    | Version of string
+[<RequireQualifiedAccess>]
+module String =
+    let split (separator: string) (value: string) =
+        value.Split([| separator |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
 
-    member this.Value =
+[<CustomComparison>]
+[<StructuralEquality>]
+type Version =
+    | Version4 of int * int * int * int
+    | Version3 of int * int * int
+    | Version2 of int * int
+
+    member this.toString() =
         match this with
-        | Version value -> value
+        | Version4(major, minor, build, revision) -> $"{major}.{minor}.{build}.{revision}"
+        | Version3(major, minor, build) -> $"{major}.{minor}.{build}"
+        | Version2(major, minor) -> $"{major}.{minor}"
+
+    member this.asTuple() =
+        match this with
+        | Version4(major, minor, build, revision) -> (major, minor, build, revision)
+        | Version3(major, minor, build) -> (major, minor, build, 0)
+        | Version2(major, minor) -> (major, minor, 0, 0)
+
+    static member parse versionString =
+
+        let parseInt (s: string) =
+            match Int32.TryParse(s) with
+            | true, value -> Some value
+            | false, _ -> None
+
+        let segments = versionString |> String.split "." |> List.traverseOptionM parseInt
+
+        match segments with
+        | Some [ major; minor; build; revision ] -> Version4(major, minor, build, revision)
+        | Some [ major; minor; build ] -> Version3(major, minor, build)
+        | Some [ major; minor ] -> Version2(major, minor)
+        | _ -> failwith $"Invalid version string: %A{versionString}"
+
+    interface IComparable with
+        member this.CompareTo(obj: obj) =
+            match obj with
+            | :? Version as other ->
+                let (major, minor, build, revision) = this.asTuple ()
+                let (otherMajor, otherMinor, otherBuild, otherRevision) = other.asTuple ()
+
+                match major.CompareTo(otherMajor) with
+                | 0 ->
+                    match minor.CompareTo(otherMinor) with
+                    | 0 ->
+                        match build.CompareTo(otherBuild) with
+                        | 0 -> revision.CompareTo(otherRevision)
+                        | result -> result
+                    | result -> result
+                | result -> result
+
+            | _ -> failwith $"Cannot compare %A{this} with %A{obj}"
 
 type CentralPackageReference = {
     PackageId: PackageId
@@ -30,7 +83,7 @@ let saveXmlWithoutDeclaration (document: XDocument) (fileName: string) =
     xmlSettings.Indent <- true
     xmlSettings.Encoding <- utf8Encoding
 
-    use streamWriter = new StreamWriter(fileName, false, utf8Encoding) 
+    use streamWriter = new StreamWriter(fileName, false, utf8Encoding)
     use xmlWriter = XmlWriter.Create(streamWriter, xmlSettings)
 
     document.Save(xmlWriter)
@@ -53,11 +106,11 @@ let currentCentralPackageReferences =
                     match child.Name.LocalName with
                     | "PackageVersion" ->
                         let packageId = child.Attribute(XName.Get("Include")).Value
-                        let version = child.Attribute(XName.Get("Version")).Value
+                        let version = child.Attribute(XName.Get("Version")).Value |> Version.parse
 
                         {
                             PackageId = PackageId packageId
-                            Version = Version version
+                            Version = version
                         }
                     | _ -> failwith $"Unexpected element %A{child}"
                 )
@@ -71,7 +124,7 @@ let projects =
     Directory.GetFiles(".", "*.?sproj", SearchOption.AllDirectories) |> Seq.toList
 
 if Environment.GetCommandLineArgs() |> Array.contains "--clean" then
-    
+
     projects
     |> List.iter (fun project ->
         let projectDocument = XDocument.Load(project)
@@ -81,7 +134,11 @@ if Environment.GetCommandLineArgs() |> Array.contains "--clean" then
         |> List.iter (fun packageReference ->
             let packageId = packageReference.Attribute(XName.Get("Include")).Value
 
-            if not <| (currentCentralPackageReferences |> Seq.exists (fun reference -> reference.PackageId.Value = packageId)) then
+            if
+                not
+                <| (currentCentralPackageReferences
+                    |> Seq.exists (fun reference -> reference.PackageId.Value = packageId))
+            then
                 packageReference.Remove()
         )
 
@@ -105,13 +162,13 @@ let newCentralPackageReferences =
                 match versionAttribute with
                 | None -> None
                 | Some versionAttribute ->
-                    let version = versionAttribute.Value
+                    let version = versionAttribute.Value |> Version.parse
 
                     versionAttribute.Remove()
 
                     Some {
                         PackageId = PackageId packageId
-                        Version = Version version
+                        Version = version
                     }
             )
 
@@ -123,8 +180,9 @@ let newCentralPackageReferences =
 
 let allPackageReferences =
     (currentCentralPackageReferences @ newCentralPackageReferences)
-    |> Seq.distinct
-    |> Seq.sortBy (_.PackageId.Value.ToLowerInvariant())
+    |> Seq.groupBy (_.PackageId.Value.ToLowerInvariant())
+    |> Seq.sortBy (fst)
+    |> Seq.map (fun (_, references) -> references |> Seq.maxBy (fun reference -> reference.Version))
     |> Seq.toList
 
 let newCentralPackageReferencesContent =
@@ -141,7 +199,7 @@ let newCentralPackageReferencesContent =
                         XElement(
                             "PackageVersion",
                             XAttribute(XName.Get("Include"), reference.PackageId.Value),
-                            XAttribute(XName.Get("Version"), reference.Version.Value)
+                            XAttribute(XName.Get("Version"), reference.Version.toString ())
                         )
                     )
                 )
